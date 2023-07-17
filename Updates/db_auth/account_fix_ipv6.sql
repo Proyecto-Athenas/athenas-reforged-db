ebe43a4346b0a0da31e1e31bafd44f4ba0cd46b9
Jose Joya <ing.jose.joya@gmail.com>
2023-07-17 17:31:09 -0500

    fix: Wrong update syntax

--- Updates/db_auth/account_fix_ipv6.sql
+++ Updates/db_auth/account_fix_ipv6.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `account` CHANGE COLUMN `last_login` `last_login` timestamp NULL,
+ALTER TABLE `account` CHANGE COLUMN `last_login` `last_login` timestamp NULL;
 ALTER TABLE `account` CHANGE COLUMN `last_ip` `last_ip` VARCHAR(40);
