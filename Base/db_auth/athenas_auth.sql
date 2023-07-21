2230ad920d8bdba74f9f297f0dfbeb4f5cc0b0de
Nefertumm <nefertum.dev@protonmail.com>
2023-07-21 03:20:46 -0300

    feat(DB): Support for MySQL 8.0

--- Base/db_auth/athenas_auth.sql
+++ Base/db_auth/athenas_auth.sql
@@ -30,7 +30,7 @@ CREATE TABLE `account` (
   `failed_logins` int(10) unsigned NOT NULL DEFAULT '0',
   `lock_country` varchar(2) DEFAULT '00',
   `locked` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `last_login` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  `last_login` TIMESTAMP NULL DEFAULT NULL,
   `online` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `expansion` tinyint(3) unsigned NOT NULL DEFAULT '3',
   `mutetime` bigint(20) NOT NULL DEFAULT '0',
@@ -98,7 +98,7 @@ DROP TABLE IF EXISTS `account_friends`;
 CREATE TABLE `account_friends` (
   `id` int(11) unsigned NOT NULL DEFAULT '0',
   `friend_id` int(11) unsigned NOT NULL DEFAULT '0',
-  `bind_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Bring Date',
+  `bind_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Bring Date',
   `expire_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Expire Date',
   PRIMARY KEY (`id`,`friend_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 PACK_KEYS=0 COMMENT='Stores accounts for Refer-a-Friend System.';
