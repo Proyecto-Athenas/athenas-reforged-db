e22ab41426e76d988fd19f3eee100f266e0c6e8d
Jose Joya <ing.jose.joya@gmail.com>
2023-07-28 00:57:01 -0500

    fix: updated Athenas coins item names

---
+++ Updates/db_world/fix-athenas-coins-names.sql
@@ -0,0 +1,8 @@
+UPDATE `item_template` SET `name` = 'Athenas Coins - 10' WHERE `entry` = 100601;
+UPDATE `item_template` SET `name` = 'Athenas Coins - 20' WHERE `entry` = 100602;
+UPDATE `item_template` SET `name` = 'Athenas Coins - 50' WHERE `entry` = 100603;
+UPDATE `item_template` SET `name` = 'Athenas Coins - 100' WHERE `entry` = 100604;
+UPDATE `locales_item` SET `name_loc6` = 'Athenas Coins - 10', `name_loc7` = 'Athenas Coins - 10' WHERE `entry` = 100601;
+UPDATE `locales_item` SET `name_loc6` = 'Athenas Coins - 20', `name_loc7` = 'Athenas Coins - 20' WHERE `entry` = 100602;
+UPDATE `locales_item` SET `name_loc6` = 'Athenas Coins - 50', `name_loc7` = 'Athenas Coins - 50' WHERE `entry` = 100603;
+UPDATE `locales_item` SET `name_loc6` = 'Athenas Coins - 100', `name_loc7` = 'Athenas Coins - 100' WHERE `entry` = 100604;
